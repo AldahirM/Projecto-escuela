65480ab8f586666d7bfb6183b41c10b94495d578
AlejandroTejedaM <alex2227@hotmail.com>
2022-05-18 20:56:41 -0500

    Se implementaron los metodos find, update, delete, consultaInd de la clase CRUD

--- src/myprojectoescuela/CRUD.java
+++ src/myprojectoescuela/CRUD.java
@@ -9,14 +9,14 @@ public class CRUD {
         grupo = new Vehiculo[100];
         indice = 0;
     }
-        
+
     /**
      * @return the vehiculo
      */
     public Vehiculo[] getVehiculo() {
         return grupo;
     }
-    
+
     /**
      * @param grupo the grupo to set
      */
@@ -49,5 +49,38 @@ public class CRUD {
             cadena += grupo[i].toString() + "\n";
         }
         return cadena;
+    }
+
+    public int find(Vehiculo vehiculo) {
+        for (int i = 0; i < indice; i++) {
+            if (grupo[i].compareTo(vehiculo) == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void update(Vehiculo vehiculo) {
+        int ind = find(vehiculo);
+        if (ind > -1) {
+            grupo[ind] = vehiculo;
+        }
+    }
+
+    public void delete(Vehiculo vehiculo) {
+        int ind = find(vehiculo);
+        if (ind > -1) {
+            for (int i = ind; i < indice - 1; i++) {
+                grupo[i] = grupo[i + 1];
+            }
+            indice--;
+        }
+    }
+
+    public Vehiculo consultaInd(int ind) {
+        if (ind > 0 && ind < indice) {
+            return grupo[ind];
+        }
+        return null;
     }
 }
