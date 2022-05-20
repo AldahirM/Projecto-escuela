1a4b72d109deb81bfc4585fc794938da107ea797
FerSosa <>
2022-05-20 10:01:38 -1200

    holi

---
+++ src/myprojectoescuela/Agencia.java
@@ -0,0 +1,25 @@
+package myprojectoescuela;
+
+public class Agencia {
+
+    public static void main(String[] args) {
+        Vehiculo vehiculo;
+        Motocicleta motocicleta;
+        Automovil automovil;
+        AutoElectrico autoElectrico;
+        CRUD crud = new CRUD();
+        int indice = 0;
+        int opc;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
---
+++ src/myprojectoescuela/Motocicleta.java
@@ -0,0 +1,64 @@
+package myprojectoescuela;
+
+public class Motocicleta extends Vehiculo {
+
+    private float potencia;
+    private byte rodada;
+
+    public Motocicleta(float potencia, byte rodada, String noSerie, String marca, String modelo, char color) {
+        super(noSerie, marca, modelo, color);
+        setPotencia(potencia);
+        setRodada(rodada);
+    }
+
+    public Motocicleta() {
+        this(1f, (byte) 1, "Sin numero de serie", "Sin marca", "Sin modelo", 'A');
+    }
+
+    /**
+     * @return the potencia
+     */
+    public float getPotencia() {
+        return potencia;
+    }
+
+    /**
+     * @param potencia the potencia to set
+     */
+    public void setPotencia(float potencia) {
+        this.potencia = potencia > 0 ? potencia : 1;
+    }
+
+    /**
+     * @return the rodada
+     */
+    public byte getRodada() {
+        return rodada;
+    }
+
+    /**
+     * @param rodada the rodada to set
+     */
+    public void setRodada(byte rodada) {
+        this.rodada = rodada > 0 ? rodada : 1;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + "Motocicleta{" + "potencia=" + potencia + ", rodada=" + rodada + '}';
+    }
+
+    @Override
+    public double costoVehiculo() {
+
+        return 0;
+    }
+
+    @Override
+    public String combustible() {
+
+        return null;
+
+    }
+}
